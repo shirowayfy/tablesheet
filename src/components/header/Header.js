e75a2defd27dd2f506ec1dc6646430a98e7f4f0c
sorawayfy <sorawayfy@gmail.com>
2020-08-27 12:49:19 +0500

    Finish ordinary framework functional

---
+++ src/components/header/Header.js
@@ -0,0 +1,24 @@
+import {ExcelComponent} from '@core/ExcelComponent';
+
+export class Header extends ExcelComponent {
+  static className = 'excel__header'
+
+  toHTML() {
+    return `
+      <div class="header__input">
+
+                <input type="text" class="input" value="New Table">
+
+                <i class="material-icons">
+                    tab
+                </i>
+
+            </div>
+
+            <div class="nav">
+                <div class="button">Delete</div>
+                <div class="button">Exit</div>
+            </div>
+    `
+  }
+}
---
+++ src/components/toolbar/Toolbar.js
@@ -0,0 +1,80 @@
+import {ExcelComponent} from '@core/ExcelComponent';
+
+export class Toolbar extends ExcelComponent {
+  static className = 'excel__toolbar'
+
+  constructor($root) {
+    super($root, {
+      name: 'Toolbar',
+      listeners: ['click']
+    });
+  }
+
+  toHTML() {
+    return `
+      <div class="button">
+                <i class="material-icons">
+                    format_align_left
+                </i>
+            </div>
+
+            <div class="button">
+                <i class="material-icons">
+                    format_align_center
+                </i>
+            </div>
+
+            <div class="button">
+                <i class="material-icons">
+                    format_align_right
+                </i>
+            </div>
+
+            <div class="button">
+                <i class="material-icons">
+                    format_bold
+                </i>
+            </div>
+
+            <div class="button">
+                <i class="material-icons">
+                    format_italic
+                </i>
+            </div>
+
+            <div class="button">
+                <i class="material-icons">
+                    format_underline
+                </i>
+            </div>
+
+            <div class="button__list">
+
+                <div class="current">12</div>
+
+                <ul class="size-list">
+                    <li class="size-list__item">13</li>
+                    <li class="size-list__item">14</li>
+                    <li class="size-list__item">15</li>
+                    <li class="size-list__item">16</li>
+
+                </ul>
+            </div>
+
+            <div class="button__fonts">
+                <div class="current">Roboto</div>
+
+                <ul class="fonts-list">
+                    <li class="fonts-list__item">Ninito</li>
+                    <li class="fonts-list__item">Georgia</li>
+                    <li class="fonts-list__item">Another</li>
+                    <li class="fonts-list__item">Another</li>
+                </ul>
+            </div>
+    `
+  }
+
+  onClick(event) {
+    console.log(event.target)
+  }
+}
