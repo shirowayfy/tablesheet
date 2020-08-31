3884995dad1316d27496bc5e8edfed89d72a7574
sorawayfy <sorawayfy@gmail.com>
2020-08-31 17:54:58 +0500

    Finish Table Resize Logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -4,6 +4,7 @@ class Dom {
       ? document.querySelector(selector)
       : selector
   }
+
   html(html) {
     if (typeof html === 'string') {
       this.$el.innerHTML = html
@@ -11,10 +12,12 @@ class Dom {
     }
     return this.$el.outerHTML.trim()
   }
+
   clear() {
     this.html('')
     return this
   }
+
   append(node) {
     if (node instanceof Dom) {
       node = node.$el
@@ -26,11 +29,35 @@ class Dom {
     }
     return this
   }
+
   on(eventType, callback) {
     this.$el.addEventListener(eventType, callback)
   }
+
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => {
+      this.$el.style[key] = styles[key]
+    })
   }
 }
 
