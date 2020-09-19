ebd7ae7a674305d8b2104d43c8039e17c33d6e79
sorawayfy <sorawayfy@gmail.com>
2020-09-19 23:48:22 +0500

    preload optimization

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import {ExcelPage} from '@/pages/ExcelPage';
 window.onload = () => {
   const $preloader = document.querySelector('.preloader')
   $preloader.style.display = 'none'
+  setTimeout(() => {
+    new Router('#app', {
+      dashboard: DashboardPage,
+      excel: ExcelPage
+    }, 100)
+  })
 }
-new Router('#app', {
-  dashboard: DashboardPage,
-  excel: ExcelPage
-})
+
